b06431d55e184466e3cb71c1111f9abefcd5fc19
JuneKim0712 <Junwoo.kim0712@gmail.com>
2020-12-13 09:35:47 +0900

    Git Init

---
+++ Main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot
+
+global golden_ratio, sr5
+
+golden_ratio = 1.61803398875
+sr5 = 2.23606797749979
+
+def binetFormula(n): return ((golden_ratio ** n) - (golden_ratio ** n))/sr5
