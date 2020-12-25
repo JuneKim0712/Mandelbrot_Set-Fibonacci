3c2d80dbd4d96cbb0d071037a21faa7cbb1c079f
JuneKim0712 <Junwoo.kim0712@gmail.com>
2020-12-25 18:30:19 +0900

    converted innto class and fixed aspect ratio and made many more features

--- mandelbrot_iterations.py
+++ mandelbrot_iterations.py
@@ -2,60 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-global n
-def ms(c):
-    t = 10000
-    z = np.array([0], dtype=complex)
-    an = complex(0)
-    while t > 0:
-        t -= 1
-        an = an ** 2 + c
-        if (abs(an.real) >= 1 or abs(an.imag) >= 1):
-            return [False, z]
-        if an in z[:-1]:
-            return [True, z]
-        z = np.append(z, an)
-    return [True, z]
- 
-def click(event):
-    try:
-        del n
-        print(1)
-    except:
-        global n
-        n=plt.connect('motion_notify_event', input)
-        print(2)
-    return
- 
-def input(event):
-    if event.xdata == None:
-        return
-    x = event.xdata
-    
-    y = event.ydata
-    plt.cla()
-    isStable, d = ms(complex(x, y))
-    if isStable == True:
-        color='black'
+class msi:
+    def __init__(self):
+        self.clicked=False
         plt.title('stable')
-    else:
-        color='red'
-        plt.title('unstable')
-    plt.plot([-1, 1], [0, 0], color='black')
-    plt.plot([0, 0], [1, -1], color='black')
-    plt.plot(d.real, d.imag, '-o', markersize=6, markerfacecolor='grey', color=color)
-    plt.plot(x, y, 'ro', color='red', markersize=6)
-    event.canvas.draw()
+        plt.plot([-1, 1], [0, 0], color='black')
+        plt.plot([0, 0], [1, -1], color='black')
+        plt.axes().set_aspect('equal')
+        plt.connect('button_press_event', self.click)
+        plt.connect('motion_notify_event', self.input)
+        plt.show()
+        return
+
+    def ms(self, c):
+        t = 1000
+        z = np.array([0], dtype=complex)
+        an = complex(0)
+        while t > 0:
+            t -= 1
+            an = an ** 2 + c
+            if (abs(an.real) >= 1 or abs(an.imag) >= 1):
+                return [False, z]
+            if an in z[:-1]:
+                return [True, z]
+            z = np.append(z, an)
+        return [True, z]
  
-def p(c):
-    plt.title('stable')
-    plt.plot([-1, 1], [0, 0], color='black')
-    plt.plot([0, 0], [1, -1], color='black')
-    d = ms(c)
-    plt.plot(d[1].real, d[1].imag, '-o', markersize=6, markerfacecolor='gray', color='black')
-    plt.plot(c.real, c.imag, 'ro', color='red', markersize=6)
-    plt.connect('button_press_event', click)
-    plt.show()
+    def click(self, event):
+        if self.clicked == True:
+            self.clicked = False
+        elif event.dblclick==True:
+            self.clicked = True
+        return
+ 
+    def input(self, event):
+        if event.xdata == None or not self.clicked:
+            return
+        x = event.xdata
+        y = event.ydata
+        plt.cla()
+        isStable, d = self.ms(complex(x, y))
+        if isStable == True:
+            color='black'
+            plt.title('stable')
+        else:
+            color='red'
+            plt.title('unstable')
+        plt.plot([-1, 1], [0, 0], color='black')
+        plt.plot([0, 0], [1, -1], color='black')
+        plt.plot(d.real, d.imag, '-o', markersize=6, markerfacecolor='grey', color=color)
+        plt.plot(x, y, 'ro', color='red', markersize=6)
+        event.canvas.draw()
+        return
+
 
 if __name__ == "__main__":
-    p(0)
+    a=msi()
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.plot([1], [1], 'ro', markerfacecolor='')
